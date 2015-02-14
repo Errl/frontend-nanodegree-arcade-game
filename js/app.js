777840db77763c03b9226bc0df2fc58df85edc00
Hanane Moshe <hananemo@gmail.com>
2015-02-13 22:14:12 -0500

    Added Player Object. Added code to both Player and Enemy. Canvas display with 3 bugs that move

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 // Enemies our player must avoid
-var Enemy = function() {
+
+var bugStartPos = [60, 140, 226];
+var Enemy = function () {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y = bugStartPos[Math.floor(Math.random() * 3)];
+
 }
 
 // Update the enemy's position, required method for game
@@ -14,6 +19,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += (20 * dt);
+
 }
 
 // Draw the enemy on the screen, required method for game
@@ -24,8 +31,31 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function () {
+    this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y = 200;
+}
 
+Player.prototype.update = function (dt) {
 
+}
+
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function () {
+
+}
+
+var enemy = new Enemy();
+var enemy2 = new Enemy();
+var enemy3 = new Enemy();
+
+var allEnemies = [enemy2, enemy3, enemy];
+
+var player = new Player();
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
